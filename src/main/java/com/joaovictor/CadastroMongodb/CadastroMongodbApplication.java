a54c653f846bb7608943dbeb98a7492078d07109
João Victor <jomiem2014@gmail.com>
2022-10-25 18:26:09 -0400

    Primeiro commit - projeto criado

---
+++ src/main/java/com/joaovictor/CadastroMongodb/CadastroMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.joaovictor.CadastroMongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaovictor/CadastroMongodb/CadastroMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaovictor.CadastroMongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
